67bf61d6a8f540ee6dbd74f289514b31f4ae7eb5
JKaaij <jkaaij5651@student.landstede.nl>
2023-02-21 11:41:22 +0100

    Placeholder and spikes added. You can set the starting speed per pixel

---
+++ GXPEngine/Block/Spike.cs
@@ -0,0 +1,34 @@
+﻿using GXPEngine;
+using GXPEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiledMapParser;
+
+public class Spike : AnimationSpriteAddOn
+{
+    public event Action GroundSpawnEvent;
+    public event Action BlockObstacleSpawnEvent;
+    public event Action GroundDestroyEvent;
+
+    public static bool spawnObstacle = false;
+    public static int abilityToSpawnCooldown = 2;
+    public static bool canSpawnObstacle = true;
+    private double xSpeed = DesignerClass.xStartingSpeed;
+
+    public Spike(string fileName, int colums, int rows, TiledObject spikeObject = null) : base(fileName, colums, rows, -1, false, true)
+    {
+        if (spikeObject != null)
+        {
+
+        }
+    }
+
+    void Update()
+    {
+        x -= (float)xSpeed;
+        xSpeed += DesignerClass.XSpeedUp;
+    }
+}
